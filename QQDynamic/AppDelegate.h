fda1b36b07ade6b9b7feaf96834f7d8b00e46305
lysongzi <lysongzi.hnu@gmail.com>
2017-02-03 18:24:10 +0800

    create project
    first commit about project.

---
+++ QQDynamic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQDynamic
+//
+//  Created by lysongzi on 2017/2/3.
+//  Copyright © 2017年 lysongzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
